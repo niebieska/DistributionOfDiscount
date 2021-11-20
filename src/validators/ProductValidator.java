e34bad99789993b7c100691ce13b3f00767a21bd
Paulina Wątroba <paulina@watroba.com.pl>
2021-11-20 16:36:54 +0100

    ProductValidator validate method impl + error solved

--- src/validators/ProductValidator.java
+++ src/validators/ProductValidator.java
@@ -3,10 +3,19 @@ package validators;
 import models.Product;
 import models.ValidationResult;
 
-public  class ProductValidator implements IValidator{
+import java.math.BigDecimal;
+
+public class ProductValidator implements IValidator<Product> {
 
     @Override
-    public ValidationResult validate(Object o) { // with Product p is superclass error
-        return null;
+    public ValidationResult validate(Product p) {
+        ValidationResult validationResult = new ValidationResult(ValidationResult.Status.PASS);
+        BigDecimal productPrice = p.getPrice();
+
+        if (productPrice.signum() <= 0) {
+            validationResult.setReason("Price is less or equal to 0");
+            validationResult.setStatus(ValidationResult.Status.ERROR);
+        }
+        return validationResult;
     }
 }
