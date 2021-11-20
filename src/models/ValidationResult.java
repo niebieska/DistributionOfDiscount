3911452591a96b3fab2f7d597b6a68d38ed8bb15
Paulina Wątroba <paulina@watroba.com.pl>
2021-11-20 16:35:02 +0100

    New field + getters and setters in ValidationResult model class

--- src/models/ValidationResult.java
+++ src/models/ValidationResult.java
@@ -2,6 +2,28 @@ package models;
 
 public class ValidationResult {
 
- enum status {PASS, WARNING, ERROR}
- String reason;
+ public enum Status {PASS, WARNING, ERROR}
+ private String reason;
+ private Status status;
+
+ public ValidationResult(Status status)
+ { this.status =status;
+
+ }
+
+ public String getReason() {
+  return reason;
+ }
+
+ public void setReason(String reason) {
+  this.reason = reason;
+ }
+
+ public Status getStatus() {
+  return status;
+ }
+
+ public void setStatus(Status status) {
+  this.status = status;
+ }
 }
