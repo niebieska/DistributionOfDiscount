68fba4365b0dc554f33a4423af3fd968276a4ae1
Paulina Wątroba <paulina@watroba.com.pl>
2021-11-22 00:03:34 +0100

    Blank Service test class

---
+++ tests/SimpleDiscountServiceTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleDiscountServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setProducts() {
+    }
+
+    @Test
+    void setDiscount() {
+    }
+
+    @Test
+    void getDiscountedProducts() {
+    }
+}
